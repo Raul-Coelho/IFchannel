88d4086e28b4bbfe57b2b0727b57eb98964f186e
Romulo Pereira <romulo.pereiracz@gmail.com>
2019-03-18 10:00:13 -0300

    Adicionando metodo de excluir comentario no commentDao

--- src/main/java/com/ifpb/model/dao/CommentDao.java
+++ src/main/java/com/ifpb/model/dao/CommentDao.java
@@ -1,11 +1,11 @@
 package com.ifpb.model.dao;
 
-import com.datastax.driver.core.BoundStatement;
-import com.datastax.driver.core.PreparedStatement;
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.Session;
+import com.datastax.driver.core.*;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.ifpb.connection.CassandraConnection;
 import com.ifpb.model.entidades.Comment;
+
+import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
 
 public class CommentDao {
     private Session session;
@@ -19,6 +19,14 @@ public class CommentDao {
         PreparedStatement statement = session.prepare("INSERT INTO comment (id, coment, username) VALUES (?,?,?)");
         BoundStatement bound = statement.bind(comment.getId(),comment.getcoment(),comment.getUsername());
         ResultSet rs = session.execute(bound);
+        CassandraConnection.closeConnection();
+        session.close();
+        return rs.wasApplied();
+    }
+    public boolean excluirComentario(int id){
+        session = CassandraConnection.getConnection();
+        Statement statement = QueryBuilder.delete().from("comment").where(eq("id",id));
+        ResultSet rs = session.execute(statement);
         CassandraConnection.closeConnection();
         session.close();
         return rs.wasApplied();
