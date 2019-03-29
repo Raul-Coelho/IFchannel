05bb76905f7061c92ffa65d5ca620877f2130627
Vindrys <raulcc50@gmail.com>
2019-03-28 22:34:37 -0300

    Pagina savePost sem mapa

---
+++ src/main/java/com/ifpb/controller/bean/PostBean.java
@@ -0,0 +1,28 @@
+package com.ifpb.controller.bean;
+
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "postBean")
+@ViewScoped
+public class PostBean {
+
+
+
+
+    public  PostBean(){
+
+    }
+
+    @PostConstruct
+    public void init(){
+
+    }
+
+    public String openSavePost(){
+        return "savePost";
+    }
+
+}
