a5ed9d11cb14c54f98348c6b104b07ebac3b7dbf
Vindrys <raulcc50@gmail.com>
2019-03-30 19:52:56 -0300

    FollowBean

--- src/main/java/com/ifpb/view/App.java
+++ src/main/java/com/ifpb/view/App.java
@@ -1,6 +1,8 @@
 package com.ifpb.view;
 
 import com.ifpb.controller.servico.PostService;
+import com.ifpb.model.dao.RelashionshipDao;
+import com.ifpb.model.entidades.User;
 
 import java.sql.SQLException;
 
@@ -10,20 +12,20 @@ public class App {
 
 //        System.out.println(dao.save(new User(0,"lucas1", "321","Lucas","M","","Fon","1","PB","5890000","889999000","102")));
 
-//        RelashionshipDao dao = new RelashionshipDao();
-//        User user = new User(1,"aluno","romulo@hotmail.com","201622010060",
-//                "123","Romulo","M","","fon","cz",
-//                "pb","58900000","190","940");
-//        User user1 = new User(2,"aluno","raul@hotmail.com","201622010066",
-//                "123","Raul","M","","fon","cz",
-//                "pb","58900000","190","940");
-//       //dao.saveUser(user1);
-//        dao.saveUser(user1);
-        //dao.createRelashionship(user,user1);
+        RelashionshipDao dao = new RelashionshipDao();
+        User user = new User(1,"aluno","romulo@hotmail.com","201622010060",
+                "123","Romulo","M","","fon","cz",
+                "pb","58900000","190","940");
+        User user1 = new User(2,"aluno","raul@hotmail.com","201622010066",
+                "123","Raul","M","","fon","cz",
+                "pb","58900000","190","940");
+        System.out.println(dao.saveUser(user));
+        System.out.println(dao.saveUser(user1));
+//        dao.createRelashionship(user,user1);
 
-        PostService service = new PostService();
-
-        System.out.println(service.list(2));
+//        PostService service = new PostService();
+//
+//        System.out.println(service.list(2));
 
 
     }
